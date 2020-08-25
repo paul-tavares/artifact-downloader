0a2e45edf83729cbabe9e1e58f3cac6837825723
Paul Tavares <paul.tavares@elastic.co>
2020-08-25 14:59:53 -0400

    updated docs + help option to cli

--- README.md
+++ README.md
@@ -3,7 +3,30 @@ Download and view Endpoint Security user generated artifacts
 
 ## Usage
 
+Use `npx` to run the utility with no installation required:
 
+```bash
+npx paul-tavares/artifact-downloader
+```
 
 ## Install
 
+Install globally and use the registered `artifact-downloader` executable
+
+```bash
+npm i -g paul-tavares/artifact-downloader
+```
+
+Run it:
+
+```bash
+artifact-downloader
+```
+
+## Uninstall
+
+Uninstall it from the global space:
+
+```bash
+npm uninstall -g artifact-downloader
+```
--- cli/cli.js
+++ cli/cli.js
@@ -2,26 +2,35 @@ import minimist from 'minimist'
 import {Kibana, promptUser} from "../lib";
 import {red, bold} from 'kleur'; // reuse from prompts
 
+const cliOptions = {
+    default: {
+        'kibana-url': 'http://localhost:5601',
+        'kibana-user': 'elastic',
+        'kibana-password': 'changeme'
+    },
+    alias: {
+        'kibana-url': 'kibanaUrl',
+        'kibana-user': 'kibanaUser',
+        'kibana-password': 'kibanaPassword',
+        'h': 'help'
+    },
+    boolean: [ 'help', 'h' ]
+};
+
 const run = async () => {
+    const runOptions = minimist(process.argv.slice(2), cliOptions);
+
+    if (runOptions.help) {
+        console.log(getHelp());
+        process.exit(0);
+    }
+
     console.log(`
 ${getHeader()}
 ${red(`
   ${bold('WARNING!')}  USE OF THIS UTILITY WILL CREATE A FLEET AGENT.
             FOR DEVELOPMENT AND TESTING PURPOSES ONLY!`)}
 `);
-
-    const runOptions = minimist(process.argv.slice(2), {
-        default: {
-            'kibana-url': 'http://localhost:5601',
-            'kibana-user': 'elastic',
-            'kibana-password': 'changeme'
-        },
-        alias: {
-            'kibana-url': 'kibanaUrl',
-            'kibana-user': 'kibanaUser',
-            'kibana-password': 'kibanaPassword'
-        }
-    });
 
     const kibana = new Kibana(runOptions);
     const userSelections = await promptUser(kibana);
@@ -58,6 +67,13 @@ ${getSeparator()}
 const getSeparator = () => '-------------------------------------------------------------------';
 
 const getHeader = () => '-[ ENDPOINT KIBANA ARTIFACT DOWNLOADER ]---------------------------';
+
+const getHelp = () => (`
+artifact-downloader [options]
+
+Options:
+${Object.keys(cliOptions.default).map(opt => `    --${opt}`).join("\n")}
+`);
 
 //==========[ RUN ]=================================
 run();
